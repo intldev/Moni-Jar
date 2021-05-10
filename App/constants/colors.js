3d7463f74162e9b400ad365c284d3cb441bcec44
intldev <intldevsllc@gmail.com>
2021-05-10 23:23:32 +0530

    feat: hexagonal checkbox

--- App/constants/colors.js
+++ App/constants/colors.js
@@ -6,9 +6,10 @@ export default {
     fbBlue: '#4267B2',
     blue: '#2EC4FF',
     secondary: {
-        grey: 'rgb(56, 56, 56)',
+        grey: 'rgba(56, 56, 56,1)',
         lightGrey: '#E5E5E5',
         darkGradient: 'rgba(30, 30, 30, 0.4)',
-        danger: '#e37448'
+        danger: '#e37448',
+        checkboxBlue: 'rgba(46,196,255,1)'
     }
 }
