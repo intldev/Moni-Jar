f13013280fcfd5dbbae51276b60c99d49777f988
intldev <intldevsllc@gmail.com>
2021-05-23 16:50:42 +0530

    chore: start a jar screen

--- src/screens/JarScreen/index.js
+++ src/screens/JarScreen/index.js
@@ -6,87 +6,109 @@ import colors from '../../constants/colors';
 import PlusIcon from '../../assets/images/plusIcon.png';
 import ListItem from '../HomeScreen/components/ListItem';
 
+// images
+import startJarImage from '../../assets/images/startJarText.png';
+import jarGraphic from '../../assets/images/jarGraphic.png';
+
 export default function JarScreen() {
 
-    const data = [
-        {
-            name: "The Spring Break Jar",
-            isProgress: true,
-            progressText: '75% SAVED',
-            progress: 0.6,
-            daysLeft: 45,
-            timeTypeText: 'Honi Bee',
-            persons: [{ name: "TS" }, { name: "CT" }, { name: "CT" }, { name: "CT" }, { name: "TS" }, { name: "PS" }, { name: "DR" }]
-        },
-        {
-            name: "The Spring Break Jar",
-            isProgress: true,
-            progressText: '75% SAVED',
-            progress: 0.6,
-            daysLeft: 45,
-            timeTypeText: 'Honi Bee',
-            persons: [{ name: "TS" }, { name: "CT" }]
-        },
-        {
-            name: "The Spring Break Jar",
-            isProgress: true,
-            progressText: '75% SAVED',
-            progress: 0.6,
-            daysLeft: 45,
-            timeTypeText: 'Honi Bee',
-            persons: [{ name: "TS" }, { name: "CT" }, { name: "TS" }, { name: "PS" }, { name: "DR" }]
-        },
-        {
-            name: "The Spring Break Jar",
-            isProgress: true,
-            progressText: '75% SAVED',
-            progress: 0.6,
-            daysLeft: 45,
-            timeTypeText: 'Honi Bee',
-            persons: [{ name: "TS" }, { name: "CT" }, { name: "DR" }]
-        },
-        {
-            name: "The Spring Break Jar",
-            isProgress: true,
-            progressText: '75% SAVED',
-            progress: 0.6,
-            daysLeft: 45,
-            timeTypeText: 'Honi Bee',
-            persons: [{ name: "TS" }, { name: "CT" }, { name: "TS" }, { name: "PS" }, { name: "DR" }]
-        },
-        {
-            name: "The Spring Break Jar",
-            isProgress: true,
-            progressText: '75% SAVED',
-            progress: 0.6,
-            daysLeft: 45,
-            timeTypeText: 'Honi Bee',
-            persons: [{ name: "TS" }, { name: "CT" }, { name: "CT" }]
-        },
+    const data = [];
 
-    ];
+    // const data = [
+    //     {
+    //         name: "The Spring Break Jar",
+    //         isProgress: true,
+    //         progressText: '75% SAVED',
+    //         progress: 0.6,
+    //         daysLeft: 45,
+    //         timeTypeText: 'Honi Bee',
+    //         persons: [{ name: "TS" }, { name: "CT" }, { name: "CT" }, { name: "CT" }, { name: "TS" }, { name: "PS" }, { name: "DR" }]
+    //     },
+    //     {
+    //         name: "The Spring Break Jar",
+    //         isProgress: true,
+    //         progressText: '75% SAVED',
+    //         progress: 0.6,
+    //         daysLeft: 45,
+    //         timeTypeText: 'Honi Bee',
+    //         persons: [{ name: "TS" }, { name: "CT" }]
+    //     },
+    //     {
+    //         name: "The Spring Break Jar",
+    //         isProgress: true,
+    //         progressText: '75% SAVED',
+    //         progress: 0.6,
+    //         daysLeft: 45,
+    //         timeTypeText: 'Honi Bee',
+    //         persons: [{ name: "TS" }, { name: "CT" }, { name: "TS" }, { name: "PS" }, { name: "DR" }]
+    //     },
+    //     {
+    //         name: "The Spring Break Jar",
+    //         isProgress: true,
+    //         progressText: '75% SAVED',
+    //         progress: 0.6,
+    //         daysLeft: 45,
+    //         timeTypeText: 'Honi Bee',
+    //         persons: [{ name: "TS" }, { name: "CT" }, { name: "DR" }]
+    //     },
+    //     {
+    //         name: "The Spring Break Jar",
+    //         isProgress: true,
+    //         progressText: '75% SAVED',
+    //         progress: 0.6,
+    //         daysLeft: 45,
+    //         timeTypeText: 'Honi Bee',
+    //         persons: [{ name: "TS" }, { name: "CT" }, { name: "TS" }, { name: "PS" }, { name: "DR" }]
+    //     },
+    //     {
+    //         name: "The Spring Break Jar",
+    //         isProgress: true,
+    //         progressText: '75% SAVED',
+    //         progress: 0.6,
+    //         daysLeft: 45,
+    //         timeTypeText: 'Honi Bee',
+    //         persons: [{ name: "TS" }, { name: "CT" }, { name: "CT" }]
+    //     },
+    // ];
     const renderItem = ({ item }) => <ListItem item={item} />;
 
     return (
         <View style={styles.container}>
-            <View style={styles.listContainer}>
-                <FlatList
-                    data={data}
-                    keyExtractor={(item, index) => index.toString()}
-                    renderItem={renderItem}
-                    contentContainerStyle={{
-                        paddingVertical: 20,
-                    }}
-                />
-            </View>
-            <View style={styles.footerContainer}>
-                <TouchableOpacity style={styles.plusIconContainer}>
-                    <Image
-                        source={PlusIcon}
-                        style={styles.plusIcon}
-                    />
-                </TouchableOpacity>
-            </View>
+            {
+                data.length === 0 ? (
+                    <View style={styles.emptyContainer}>
+                        <Image
+                            source={startJarImage}
+                            style={styles.startJarImage}
+                        />
+                        <Image
+                            source={jarGraphic}
+                            style={styles.startJarGraphic}
+                        />
+                    </View>
+                ) : (
+                    <>
+                        <View style={styles.listContainer}>
+                            <FlatList
+                                data={data}
+                                keyExtractor={(item, index) => index.toString()}
+                                renderItem={renderItem}
+                                contentContainerStyle={{
+                                    paddingVertical: 20,
+                                }}
+                            />
+                        </View>
+                        <View style={styles.footerContainer}>
+                            <TouchableOpacity style={styles.plusIconContainer}>
+                                <Image
+                                    source={PlusIcon}
+                                    style={styles.plusIcon}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                    </>
+                )
+            }
         </View>
     )
 }
@@ -111,5 +133,23 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         paddingTop: 24
+    },
+    emptyContainer: {
+        flex: 1,
+        paddingHorizontal: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingBottom: 24
+    },
+    startJarImage: {
+        width: '50%',
+        height: 70,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+    },
+    startJarGraphic: {
+        resizeMode: 'contain',
+        height: '50%',
+        alignSelf: 'center'
     }
 })
